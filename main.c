e22c62f9df3cbf3f20ad38ba18381f5de85bcdce
Vitor Loli <vgabrielloli221@gmail.com>
2025-05-16 21:36:01 -0300

    Buscar e atualizar clientes

--- main.c
+++ main.c
@@ -16,6 +16,28 @@ typedef struct produto {
 } produto;
 struct produto Produtos[10];
 
+int buscar_cliente(char nome[50], char cpf[12] , int opc) {
+    int i = 0;
+
+    if (opc == 1) {
+        while (strcmp(Clientes[i].nome, nome) != 0 ) {
+            i++;
+            if (i > 9) {
+                break;
+            }
+        }
+    }else if (opc == 2) {
+        while (strcmp(Clientes[i].cpf, cpf) != 0 ) {
+            i++;
+            if (i > 9) {
+                break;
+            }
+        }
+    }
+
+    return i;
+}
+
 void insere_cliente(){
     int opc;
     for(int i = 0; i < 10; i++){
@@ -59,21 +81,11 @@ void excluir_cliente(){
     if (opc == 1) {
         printf("Informe o nome do cliente: \n");
         scanf("%s", &nome);
-        while (strcmp(Clientes[i].nome, nome) != 0 ) {
-            i++;
-            if (i > 9) {
-                break;
-            }
-        }
+        i = buscar_cliente(nome, cpf, opc);
     }else if (opc == 2) {
         printf("Informe o cpf do cliente: EX: 12345678911\n");
         scanf("%s", &cpf);
-        while (strcmp(Clientes[i].cpf, cpf) != 0 ) {
-            i++;
-            if (i > 9) {
-                break;
-            }
-        }
+        i = buscar_cliente(nome, cpf, opc);
     } else {
         excluir_cliente();
     }
@@ -98,7 +110,54 @@ void excluir_cliente(){
 }
 
 void atualizar_cliente() {
+    char nome[50];
+    char cpf[12];
+    int i;
+    printf("Esses são os clientes cadastrados no sistema, qual deseja alterar? Informe o nome\n");
+    listar_clientes();
+    scanf("%s", &nome);
+    i = buscar_cliente(nome, cpf, 1);
 
+    if (i == 10) {
+        printf("Cliente não encontrado, informe outro");
+        atualizar_cliente();
+    }else {
+        int opc;
+        char nomeAlterado[50];
+        char cpfAlterado[12];
+
+        printf("O que deseja alterar?\n");
+        printf("1. Nome\n");
+        printf("2. CPF\n");
+        printf("3. Nome e CPF\n");
+        scanf("%d", &opc);
+
+        if (opc == 1) {
+            printf("Informe o novo nome do cliente: \n");
+            scanf("%s", &nomeAlterado);
+            strcpy(Clientes[i].nome, nomeAlterado);
+        }else if (opc == 2) {
+            printf("Informe o novo cpf do cliente: EX: 12345678911\n");
+            scanf("%s", &cpfAlterado);
+            strcpy(Clientes[i].cpf, cpfAlterado);
+        }else if (opc == 3) {
+            printf("Informe o novo nome do cliente: \n");
+            scanf("%s", &nomeAlterado);
+            printf("Informe o novo cpf do cliente: EX: 12345678911\n");
+            scanf("%s", &cpfAlterado);
+            strcpy(Clientes[i].nome, nomeAlterado);
+            strcpy(Clientes[i].cpf, cpfAlterado);
+        }
+
+        printf("Deseja atualizar outro cliente?\n");
+        printf("1. Sim\n");
+        printf("2. Não\n");
+        scanf("%d", &opc);
+
+        if (opc == 1) {
+            atualizar_cliente();
+        }
+    }
 }
 
 void insere_produto(){
@@ -143,10 +202,11 @@ int main()
      printf("1. Inserir Clientes\n");
      printf("2. Listar Clientes\n");
      printf("3. Excluir Cliente\n");
+     printf("4. Atualizar Cliente\n");
      printf("Produtos\n");
-     printf("4. Inserir Produto\n");
-     printf("5. Listar Produto\n");
-     printf("6. Excluir Produto\n");
+     printf("5. Inserir Produto\n");
+     printf("6. Listar Produto\n");
+     printf("7. Excluir Produto\n");
      scanf("%d", &opc);
 
      switch(opc){
@@ -163,9 +223,10 @@ int main()
              atualizar_cliente();
              break;
          case 5:
-             listar_produtos();
+             insere_produto();
              break;
          case 6:
+             listar_produtos();
              break;
         case 8:
             printf("Até logo!");
